bf62faee6a7ad68ae17bdb577424870c57ff4585
Vijay <vijaymukund1217@gmail.com>
2025-01-15 20:22:58 -0600

    Initial Commit

---
+++ GoalFiTests/GoalFiTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoalFiTests.swift
+//  GoalFiTests
+//
+//  Created by Vijay Venkatesan on 1/15/25.
+//
+
+import Testing
+@testable import GoalFi
+
+struct GoalFiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
