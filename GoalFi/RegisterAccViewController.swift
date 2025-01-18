77f1789d6f773d69d3debe6606510a20777b77a1
Vijay <vijaymukund1217@gmail.com>
2025-01-17 23:59:10 -0600

    Changed to button system and introduced accounts.

---
+++ GoalFi/RegisterAccViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RegisterAccViewController.swift
+//  GoalFi
+//
+//  Created by Vijay Venkatesan on 1/17/25.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+class RegisterAccViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func registerClicked(_ sender: UIButton) {
+        guard let email = emailTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        
+        Auth.auth().createUser(withEmail: email, password: password) { firebaseResult, error in
+            if let e = error {
+                print("Error")
+            }
+            else {
+                self.performSegue(withIdentifier: "goToHome", sender: self)
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
