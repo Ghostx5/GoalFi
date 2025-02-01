80e6b395ddff013ffb15cadf5ee028b11bd7ca07
Samuel Deepak <stallingbread44@gmail.com>
2025-02-01 10:44:16 -0600

    Added navigation for curriculum fragments

--- app/src/main/java/com/aspirefinance/goalfi/pages/curriculum/curriculum_navigation.kt
+++ app/src/main/java/com/aspirefinance/goalfi/pages/curriculum/curriculum_navigation.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.aspirefinance.goalfi.R
+import com.aspirefinance.goalfi.pages.homePage
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,40 +20,59 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class curriculum_navigation : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
-    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_curriculum_navigation, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment curriculum_navigation.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                curriculum_navigation().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+
+        val module_1 = view.findViewById<Button>(R.id.module_1)
+        val module_2 = view.findViewById<Button>(R.id.module_2)
+        val module_3 = view.findViewById<Button>(R.id.module_3)
+        val module_4 = view.findViewById<Button>(R.id.module_4)
+        val module_5 = view.findViewById<Button>(R.id.module_5)
+        val module_6 = view.findViewById<Button>(R.id.module_6)
+        val module_7 = view.findViewById<Button>(R.id.module_7)
+        val module_8 = view.findViewById<Button>(R.id.module_8)
+        val module_9 = view.findViewById<Button>(R.id.module_9)
+
+        module_1.setOnClickListener {
+            (activity as? homePage)?.replaceFragment(module_1())
+        }
+        module_2.setOnClickListener {
+            (activity as? homePage)?.replaceFragment(module_2())
+        }
+        module_3.setOnClickListener {
+            (activity as? homePage)?.replaceFragment(module_3())
+        }
+        module_4.setOnClickListener {
+            (activity as? homePage)?.replaceFragment(module_4())
+        }
+        module_5.setOnClickListener {
+            (activity as? homePage)?.replaceFragment(module_5())
+        }
+        module_6.setOnClickListener {
+            (activity as? homePage)?.replaceFragment(module_6())
+        }
+        module_7.setOnClickListener {
+            (activity as? homePage)?.replaceFragment(module_7())
+        }
+        module_8.setOnClickListener {
+            (activity as? homePage)?.replaceFragment(module_8())
+        }
+        module_9.setOnClickListener {
+            (activity as? homePage)?.replaceFragment(module_9())
+        }
     }
 }
